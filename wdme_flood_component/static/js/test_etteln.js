9f7983d197c1309c3331965f6f74227512cfc6b0
glewis-unexe <128075194+glewis-unexe@users.noreply.github.com>
2026-02-25 11:01:50 +0000

    Update test_etteln.js

--- wdme_flood_component/static/js/test_etteln.js
+++ wdme_flood_component/static/js/test_etteln.js
@@ -1,4 +1,3 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ2F6dGFzdGljIiwiYSI6ImNrYzA4Y2c4NjFoYnIyeHRicmZuaTgyMGQifQ.fkkbIOCwq4j70CqNeiBGcA';
 //let server_url = 'http://localhost:9999/flooding';
 let server_url = '/flooding/';
 
